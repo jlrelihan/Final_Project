d9b389925706420cc2f8012d1f186cc4b7f1db40
jlrelihan <jlreliha@ncsu.edu>
2021-12-04 17:42:07 -0500

    Created a few new columns for the data exploration page in a object - unique_teams and added a data table and bar plot.

--- NFL/server.R
+++ NFL/server.R
@@ -13,7 +13,7 @@ library(ggplot2)
 
 shinyServer(function(input, output) {
     # Bring in data set
-    super <- nflfastR::fast_scraper_schedules(2000:2020) %>%
+    super <- nflfastR::fast_scraper_schedules(1999:2020) %>%
         dplyr::filter(game_type == "SB") %>%
         dplyr::pull(game_id)
     
@@ -21,16 +21,10 @@ shinyServer(function(input, output) {
     
     # Create a cleaned up date column for just the year.
     super_bowl <- super_bowl %>% separate(game_date, into = c("game_year"), sep = "-", remove = FALSE)
-
+    # Create a new column for team vs team (home team vs away team)
+    super_bowl <- super_bowl %>% mutate(home_vs_away= paste(super_bowl$home_team,"vs ", super_bowl$away_team))
     
-    # Information for About Page
-    #create text info
-    output$purpose <- renderText({
-        #paste info out
-        paste("")
-        
-    })
-    
+    # Creating a reactive data object - this is used for data page for the data table filters.
     datasetInput <- reactive(
         if(input$show_filters == FALSE & input$yearx == "All") {
             newData <- super_bowl
@@ -62,4 +56,34 @@ shinyServer(function(input, output) {
         }
     )
     
+    # Data Exploration Page
+    
+    # Getting a table of unique teams and game years
+    unique_teams <- select(super_bowl, home_vs_away, home_score, away_score, game_year, qtr, game_seconds_remaining, home_team, away_team )
+    # Filter dataset for final score
+    unique_teams <- filter(unique_teams, qtr==4, game_seconds_remaining==0)
+    # New column shows which team home or away won the game
+    unique_teams <- mutate(unique_teams,winner= ifelse(unique_teams$home_score>unique_teams$away_score, "home","away"))
+    # New column shows which team won the game
+    unique_teams <- mutate(unique_teams,team_winner = ifelse(unique_teams$winner=="home",unique_teams$home_team,unique_teams$away_team))
+    
+    # Select only interested columns
+    unique_teams <- select(unique_teams, home_vs_away, home_score, away_score, team_winner ,game_year)
+    
+    # List of unique teams that played in the last 21 years of the Superbowl
+    output$teams <- renderDataTable({
+        unique_teams
+            })
+
+    # Bar plot showing the winning teams that played in the last 21 years of the Superbowl.
+    output$bar_1 <- renderPlot({
+        g <- ggplot(unique_teams, aes(x=team_winner))
+        g + geom_bar()
+    }
+        
+    )
+    
     })
+
+
+
--- NFL/ui.R
+++ NFL/ui.R
@@ -19,20 +19,41 @@ shinyUI(fluidPage(
     titlePanel("NFL Super Bowl Data Explorer"),
     
     # Set up Navigation Bar
-    navbarPage("NFL Data Exploration",
+    navbarPage("NFL Data Explorer",
                tabPanel("About",
-                        h3("Purpose of App:"),
-                        textOutput("purpose"),
-                        
-                        h3("Data Review:"),
-                        textOutput(""),
-                        
-                        h3("Tab Overview:"),
-                        textOutput("")
-                        # Add photo
+                        img(src="nfllogo.png",align="center",height=200,width=400),
+                        h2(strong("Purpose of App:")),
+                        h5("This app lets you explore NFL Superbowl Data over the past 20 years"),
+                        h2(strong("Data Review:")),
+                        h5("The data used for this app is from the nflfastR package. The package contains NFL play-by-play data where you can access different game types including super bowl and regular season data. You can also access many years of data back to 1999. For more information: ", a(href = "https://www.nflfastr.com/","Click here")),
+                        h2(strong("Tab Overview:")),
+                        h4("Data Exploration"),
+                            h5("On this page you can view numerical and graphical summaries of the data."),
+                        h4("Modeling"),
+                            h5("In each tab of this page you can view the three different supervised learning models."),
+                        h4("Modeling Info"),
+                            h5("On this page there are details explaining each of the three modeling approaches and their benefits and drawbacks."),
+                        h4("Model Fitting"),
+                            h5("On this page you can fit the models. You'll be able to choose some of the modeling parameters like the proportion of data used in each training and test set and variables used."),
+                        h4("Prediction"),
+                            h5("On this page you can choose one of the models for prediction."),
+                        h4("Data"),
+                            h5("Here you can scroll through the full data set, subset the data for specific columns and game years, and download the file as a csv.")
                ),
                tabPanel("Data Exploration",
                         # Numerical & Graphical Summaries
+                        sidebarLayout(
+                            sidebarPanel(
+                                
+                            ),
+                            mainPanel(
+                                h2("Final Scores for the last 21 years of the Superbowl"),
+                                h5("Use search for year or team"),
+                                DT::dataTableOutput("teams"),
+                                h2(),
+                                plotOutput("bar_1")
+                            )
+                        )
                ),
                navbarMenu("Modeling",
                           tabPanel("Model 1"),
@@ -52,13 +73,13 @@ shinyUI(fluidPage(
                         sidebarLayout(
                             sidebarPanel(
                                 selectInput("yearx", label = "Game Year", 
-                                            choices = c("All","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"), selected = "All"),
+                                            choices = c("All","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"), selected = "All"),
                                 h6("Show Filters"),
                                 checkboxInput("show_filters",label=NULL,value=FALSE),
                                 conditionalPanel("input.show_filters",
                                                  checkboxGroupInput(
                                                      'show_vars','Columns in data to show',
-                                                     c("home_team", "away_team", "season_type", "game_date","game_year","play_type"),selected=c("home_team", "away_team", "season_type", "game_date","game_year","play_type")
+                                                     c("home_team", "away_team", "season_type", "game_date","game_year","play_type","teams_playing"),selected=c("home_team", "away_team", "season_type", "game_date","game_year","play_type","teams_playing")
                                                  )),
                                 downloadButton("downloaddata","Download Data")
                             ),
