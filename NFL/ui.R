f16a613add676e09da30aa672fca62bd5cc4bb15
jlrelihan <jlreliha@ncsu.edu>
2021-12-04 13:41:26 -0500

    Updated UI so that there can be custom side bars for each tab panel (page).

--- NFL/ui.R
+++ NFL/ui.R
@@ -9,7 +9,7 @@ library(ggplot2)
 library(bslib)
 library(DT)
 
-# Define UI for application that draws a histogram
+# Shiny App
 shinyUI(fluidPage(
     
     # Adding theme
@@ -18,59 +18,55 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("NFL Super Bowl Data Explorer"),
     
-    # Side bar Layout
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("yearx", label = "Game Year", 
-                        choices = c("All","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"),
-                        selected = "All"),
-            h6("Show Filters"),
-            checkboxInput("show_filters",label=NULL,value=FALSE),
-                conditionalPanel("input.show_filters",
-                    checkboxGroupInput(
-                    'show_vars','Columns in data to show',
-                    c("home_team", "away_team", "season_type", "game_date","game_year","play_type"),selected=c("home_team", "away_team", "season_type", "game_date","game_year","play_type")
-                )),
-            downloadButton("downloaddata","Download Data")
-        ),
-        # Show a plot of the generated distribution
-        mainPanel(
-            navbarPage("NFL Data Exploration",
-                       tabPanel("About",
-                                h3("Purpose of App:"),
-                                textOutput("purpose"),
-                                
-                                h3("Data Review:"),
-                                textOutput(""),
-                                
-                                h3("Tab Overview:"),
-                                textOutput("")
-                                # Add photo
-                       ),
-                       tabPanel("Data Exploration",
-                                # Numerical & Graphical Summaries
-                       ),
-                       navbarMenu("Modeling",
-                                  tabPanel("Model 1"),
-                                  tabPanel("Model 2"),
-                                  tabPanel("Model 3")
-                       ),
-                       tabPanel("Modeling Info",
-                                # Explanation, benefits, drawbacks, math type 
-                       ),
-                       tabPanel("Model Fitting",
-                                
-                       ),
-                       tabPanel("Prediction",
-                                
-                       ),
-                       tabPanel("Data",
-                                #Scroll through data set
+    # Set up Navigation Bar
+    navbarPage("NFL Data Exploration",
+               tabPanel("About",
+                        h3("Purpose of App:"),
+                        textOutput("purpose"),
+                        
+                        h3("Data Review:"),
+                        textOutput(""),
+                        
+                        h3("Tab Overview:"),
+                        textOutput("")
+                        # Add photo
+               ),
+               tabPanel("Data Exploration",
+                        # Numerical & Graphical Summaries
+               ),
+               navbarMenu("Modeling",
+                          tabPanel("Model 1"),
+                          tabPanel("Model 2"),
+                          tabPanel("Model 3")
+               ),
+               tabPanel("Modeling Info",
+                        # Explanation, benefits, drawbacks, math type 
+               ),
+               tabPanel("Model Fitting",
+                        
+               ),
+               tabPanel("Prediction",
+                        
+               ),
+               tabPanel("Data",
+                        sidebarLayout(
+                            sidebarPanel(
+                                selectInput("yearx", label = "Game Year", 
+                                            choices = c("All","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"), selected = "All"),
+                                h6("Show Filters"),
+                                checkboxInput("show_filters",label=NULL,value=FALSE),
+                                conditionalPanel("input.show_filters",
+                                                 checkboxGroupInput(
+                                                     'show_vars','Columns in data to show',
+                                                     c("home_team", "away_team", "season_type", "game_date","game_year","play_type"),selected=c("home_team", "away_team", "season_type", "game_date","game_year","play_type")
+                                                 )),
+                                downloadButton("downloaddata","Download Data")
+                            ),
+                            mainPanel(
                                 h2("Super Bowl Data"),
-                                DT::dataTableOutput("mytable")
-                                #Subset data set (rows and columns)
-                                #Save subset data as a file (.csv)
-                       )
-            ))
+                                DT::dataTableOutput("mytable"),
+                            )
+                        )
+               )
     )
 ))
